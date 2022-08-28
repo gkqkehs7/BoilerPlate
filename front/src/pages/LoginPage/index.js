2b7a53c2e733f3bda1fe54da8e1f3f7223195765
unknown <gkqkehs7@github.com>
2022-08-29 00:46:33 +0900

    error fix

---
+++ front/src/pages/LoginPage/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const LoginPage = () => {
+  const CLIENT_ID = "4fa59b6793e017cb3c54142657950f26";
+  const REDIRECT_URI = "http://localhost:80/api/user/kakao/redirect";
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+  return (
+    <div>
+      <a id="kakao" href={KAKAO_AUTH_URL} class="btn">
+        카카오톡 로그인
+      </a>
+    </div>
+  );
+};
+
+export default LoginPage;
